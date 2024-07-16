42eaa65b6f37737603205c647f74b67b455f5381
Lilesh Bawane <lileshrocking@gmail.com>
2024-07-16 16:06:59 +0530

    initial commit

---
+++ Frontend/src/components/portfolio/PHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PNavbar from './PNavbar'
+import PBanner from './PBanner'
+import PFooter from './PFooter'
+
+function PHome() {
+  return (
+    <>
+    <PNavbar/>
+    <PBanner/>
+    <PFooter/>
+    </>
+  )
+}
+
+export default PHome
